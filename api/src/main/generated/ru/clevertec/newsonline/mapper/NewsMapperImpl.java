ff14c31615b1e630ec67c4792e1f334150eaeaf7
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-03-04 12:08:59 +0300

    add oauth authefication

--- api/src/main/generated/ru/clevertec/newsonline/mapper/NewsMapperImpl.java
+++ api/src/main/generated/ru/clevertec/newsonline/mapper/NewsMapperImpl.java
@@ -23,7 +23,7 @@ import ru.clevertec.newsonline.newService.enums.Section;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-02T23:40:39+0300",
+    date = "2025-03-04T00:31:41+0300",
     comments = "version: 1.6.2, compiler: javac, environment: Java 21.0.5 (Amazon.com Inc.)"
 )
 @Component
--- api/src/main/java/ru/clevertec/newsonline/config/SecurityBeans.java
+++ api/src/main/java/ru/clevertec/newsonline/config/SecurityBeans.java
@@ -1,10 +1,9 @@
-package ru.clevertec.cloud.config;
+package ru.clevertec.newsonline.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +12,8 @@ public class SecurityBeans {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests(authorizeHttpRequests ->
-                        authorizeHttpRequests.requestMatchers("/**")
-                .permitAll())
-
-                .httpBasic(Customizer.withDefaults())
-                .sessionManagement(sessionManagement ->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequest -> authorizeHttpRequest.anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
                 .build();
     }
-
 }
