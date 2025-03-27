61faec2dde8a6864b7f3ce024cf04ee4240e6b33
Ivan Sapozhnikov <ivan101454@gmail.com>
2025-03-27 23:28:23 +0300

    add IT news test for NewsControler

--- api/src/test/java/ru/clevertec/newsonline/controller/newcontroller/NewsControllerIT.java
+++ api/src/test/java/ru/clevertec/newsonline/controller/newcontroller/NewsControllerIT.java
@@ -1,5 +1,6 @@
 package ru.clevertec.newsonline.controller.newcontroller;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -7,23 +8,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.assertj.MockMvcTester;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @Transactional
 @SpringBootTest
@@ -109,7 +109,65 @@ class NewsControllerIT {
     @Test
     void updateNews_RequestIsValid_ReturnNoContent() throws Exception {
         //given
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.patch("/catalogue-api/news/9999999")
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.multipart("/catalogue-api/news/8918718")
+                .file(new MockMultipartFile(
+                        "newsDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "headerNews": "Тестовый заголовок",
+                                    "dateOfNews": null,
+                                    "articleId": 8918718,
+                                    "isPublished": true,
+                                    "shortDescription": "Тестовое описание",
+                                    "pictures": null,
+                                    "comments": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "categoryDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "categoryId": null,
+                                    "section": "PEOPLE",
+                                    "newsList": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "image",
+                        "test-image.jpg",
+                        MediaType.IMAGE_JPEG_VALUE,
+                        "test image content".getBytes()
+                ))
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        requestBuilder.with(new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.setMethod("PATCH");
+                return request;
+            }
+        });
+
+        //when
+        mockMvc.perform(requestBuilder)
+
+                //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNoContent()
+                );
+    }
+
+    @Test
+    void updateNews_NewsNotExist_ReturnNoContent() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.multipart("/catalogue-api/news/1111111")
                 .file(new MockMultipartFile(
                         "newsDto",
                         null,
@@ -146,15 +204,189 @@ class NewsControllerIT {
                 ))
                 .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
 
+        requestBuilder.with(new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.setMethod("PATCH");
+                return request;
+            }
+        });
+
         //when
         mockMvc.perform(requestBuilder)
 
                 //then
                 .andDo(print())
                 .andExpectAll(
+                        status().isNotFound()
+                );
+    }
+
+    @Test
+    void updateNews_RequestIsInvalid_ReturnError() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.multipart("/catalogue-api/news/8918718")
+                .file(new MockMultipartFile(
+                        "newsDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "headerNews": "",
+                                    "dateOfNews": null,
+                                    "articleId": 8918718,
+                                    "isPublished": true,
+                                    "shortDescription": "",
+                                    "pictures": null,
+                                    "comments": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "categoryDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "categoryId": null,
+                                    "section": "PEOPLE",
+                                    "newsList": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "image",
+                        "test-image.jpg",
+                        MediaType.IMAGE_JPEG_VALUE,
+                        "test image content".getBytes()
+                ))
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        requestBuilder.with(new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.setMethod("PATCH");
+                return request;
+            }
+        });
+
+        //when
+        mockMvc.perform(requestBuilder)
+
+                //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isBadRequest(),
+                        content().contentTypeCompatibleWith(MediaType.APPLICATION_PROBLEM_JSON),
+                        jsonPath("$.properties.errors").exists(),
+                        jsonPath("$.properties.errors").value(Matchers.containsInAnyOrder(
+                                "Нет описания", "Имя новости пустое"
+                        )));
+    }
+
+    @Test
+    void updateNews_RequestIsForbidden_ReturnForbidden() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.multipart("/catalogue-api/news/8918718")
+                .file(new MockMultipartFile(
+                        "newsDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "headerNews": "",
+                                    "dateOfNews": null,
+                                    "articleId": 8918718,
+                                    "isPublished": true,
+                                    "shortDescription": "",
+                                    "pictures": null,
+                                    "comments": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "categoryDto",
+                        null,
+                        MediaType.APPLICATION_JSON_VALUE,
+                        """
+                                {
+                                    "categoryId": null,
+                                    "section": "PEOPLE",
+                                    "newsList": null
+                                }
+                                """.getBytes()
+                ))
+                .file(new MockMultipartFile(
+                        "image",
+                        "test-image.jpg",
+                        MediaType.IMAGE_JPEG_VALUE,
+                        "test image content".getBytes()
+                ))
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
+
+        requestBuilder.with(new RequestPostProcessor() {
+            @Override
+            public MockHttpServletRequest postProcessRequest(MockHttpServletRequest request) {
+                request.setMethod("PATCH");
+                return request;
+            }
+        });
+
+        //when
+        mockMvc.perform(requestBuilder)
+
+                //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden()
+                        );
+    }
+
+    @Test
+    void deleteNews_NewsIsExist_ReturnNoContent() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/catalogue-api/news/8918718")
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        //when
+        mockMvc.perform(requestBuilder)
+
+        //then
+                .andDo(print())
+                .andExpectAll(
                         status().isNoContent()
                 );
     }
 
+    @Test
+    void deleteNews_UserNotAuthorized_ReturnForbidden() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/catalogue-api/news/8918718")
+                .with(jwt().jwt(builder -> builder.claim("scope", "view_catalogue")));
 
+        //when
+        mockMvc.perform(requestBuilder)
+
+        //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isForbidden()
+                );
+    }
+
+    @Test
+    void deleteNews_NewsNotExist_ReturnForbidden() throws Exception {
+        //given
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.delete("/catalogue-api/news/1111111")
+                .with(jwt().jwt(builder -> builder.claim("scope", "edit_catalogue")));
+
+        //when
+        mockMvc.perform(requestBuilder)
+
+        //then
+                .andDo(print())
+                .andExpectAll(
+                        status().isNotFound()
+                );
+    }
 }
